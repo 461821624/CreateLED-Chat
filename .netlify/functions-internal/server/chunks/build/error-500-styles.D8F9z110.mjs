9d58cbdee0ae003a5a97573759da157d7e419e6b
Song Shunshun <461821624@qq.com>
2025-09-04 09:11:08 +0800

    chore: 删除不再使用的Netlify函数内部文件和相关依赖

--- .netlify/functions-internal/server/chunks/build/error-500-styles.D8F9z110.mjs
+++
@@ -1,6 +0,0 @@
-import { e as error500_vue_vue_type_style_index_0_scoped_ac4f4f6d_lang } from './error-500-styles-1.mjs-hPGLLPLP.mjs';
-
-const error500Styles_D8F9z110 = [error500_vue_vue_type_style_index_0_scoped_ac4f4f6d_lang];
-
-export { error500Styles_D8F9z110 as default };
-//# sourceMappingURL=error-500-styles.D8F9z110.mjs.map
